792c218d23d41d9ed7090175923d2390cadb86f5
4RSIM3R <44225519+4RSIM3R@users.noreply.github.com>
2018-10-20 15:49:27 +0700

    Create connect.php

---
+++ app/models/connect.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Untuk Konfigurasi Koneksi Ke databese menggunakan Driver Mysqli
+ */
+return array(
+	'host' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'db_name' => 'db_name',
+	 );
