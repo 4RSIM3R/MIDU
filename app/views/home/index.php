ed601dd18869cc0cdf7347732b2281c8032dd4ef
4RSIM3R <44225519+4RSIM3R@users.noreply.github.com>
2018-10-20 15:53:30 +0700

    Create index.php

---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container mt-5">
+<div class="jumbotron ">
+  <h1 class="display-4">MIDU Framework</h1>
+  <p class="lead">Mini Framework PHP Kreasi Siswa SMK Telkom Darul Ulum Jombang </p>
+  <hr class="my-4">
+  <p>Karena Kita (Saya) Adalah Santri Dan Wajib Santri Itu Menguasai Teknologi Semoga Allah Meridhoi -Author This Framework (4RS!M3R)-</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Explore</a>
+  <p class="my-3 text-center"> Copyright &copy; Z4M3L</p>
+</div>
+</div>
