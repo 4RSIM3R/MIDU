face76c2e6e2b9c99d80c627b1308ba669154744
4RSIM3R <44225519+4RSIM3R@users.noreply.github.com>
2018-10-23 15:30:22 +0700

    Update connect.php

--- app/models/connect.php
+++ app/models/connect.php
@@ -1,10 +1,22 @@
 <?php
 /**
- * Untuk Konfigurasi Koneksi Ke databese menggunakan Driver Mysqli
+ * Untuk Koneksi Ke Databese
  */
-return array(
-	'host' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'db_name' => 'db_name',
-	 );
+class db
+{
+	//deklarasi parameter untuk konek ke db
+	private $host = "localhost";
+	private $user = "root";
+	private $pass = "";
+	private $db_name = "db_name";
+	function connect()
+	{
+		$connect = new mysqli($this->host, $this->user, $this->pass, 
+			$this->db_name);
+		if ($connect->connect_error) {
+			echo "Connection error : (".$connect->connect_error.")";
+		}
+
+		return $connect;
+	}
+}
